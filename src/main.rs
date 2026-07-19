5e03c96add23e080a766e68b7d3894250030eeb3
Sarah Clark <seclark@nextquestion.net>
2026-07-19 14:37:27 -0700

    feat: shared cli options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+use clapfig::ConfigArgs;
+
+#[derive(Parser)]
+#[command(name = "nmt")]
+#[command(version = "0.1.0")]
+#[command(about = "Nixpkgs Maintainer's Toolkit", long_about = None)]
+struct Cli {
+    #[arg(long, global = true)]
+    verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Manage the configuration file (gen, get, set, list).
+    Config(ConfigArgs),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = Cli::parse();
 }
